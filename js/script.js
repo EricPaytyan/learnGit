f6083d07eaab5a97136cc7a6ad2730d23888b29d
Erik <paytyaneric@gmail.com>
2025-05-07 18:09:00 +0300

    modify testForGit fn

--- js/script.js
+++ js/script.js
@@ -2,12 +2,22 @@ console.log('start lesson 1');
 
 
 function testForGit(msg) {
-    console.log(msg);
-    
+    //console.log(msg);
+    const tArr = [];
+    for (let index = 0; index < 100; index++) {
+        tArr.push('index is ' + index)
+        
+    }
+
+    console.log(tArr);
+
 }
 
 
 testForGit('hello msg 1')
+
+console.log('after startFN');
+
 
 
 
